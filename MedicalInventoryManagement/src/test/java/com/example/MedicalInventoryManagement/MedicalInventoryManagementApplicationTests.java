644db0f6fd8ba87d6024f56f8c68339e1f183e79
stephanfdo <k.stephanfdo@gmail.com>
2024-02-24 11:44:09 +0530

    adding files

---
+++ MedicalInventoryManagement/src/test/java/com/example/MedicalInventoryManagement/MedicalInventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MedicalInventoryManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalInventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
